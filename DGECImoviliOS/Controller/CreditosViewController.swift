ba91244264310104ca87a1dbeedea848feb94560
Idel <idlch.perte@gmail.com>
2023-01-13 14:03:03 -0600

    Nuevos Cambios

---
+++ DGECImoviliOS/Controller/CreditosViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CreditosViewController.swift
+//  DGECImoviliOS
+//
+//  Created by De la Cruz Soto on 13/01/23.
+//
+
+import UIKit
+import WebKit
+
+class CreditosViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
